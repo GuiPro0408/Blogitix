a3cc0f794ff38b647c8ece47c27a064da9570a3d
ophelie turenne <turenneophelie@gmail.com>
2023-03-21 00:40:56 +0400

    added info on admin dashboard for post

--- blogitix/myblog/admin.py
+++ blogitix/myblog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created_on')
+    search_fields = ['title', 'content']
+
+admin.site.register(Post, PostAdmin)
