5d77ebb07a9cb049faefb0a6dd03233acf302c78
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-03-22 23:13:15 +0400

    update view form operational

--- blogitix/myblog/views.py
+++ blogitix/myblog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post, Category
-from .forms import PostForm
+from .forms import PostForm, UpdatePostForm
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -16,11 +16,11 @@ class AddPostView(generic.CreateView):
     model = Post
     form_class = PostForm # This line adds the form to the view.
     template_name = 'add_post.html'
-    
+
     # get_initial is a special method that is used to set the initial values for the fields in the form.
     def get_initial(self):
         # Here we call the get_initial method from the parent class to
-        initial = super().get_initial() 
+        initial = super().get_initial()
         # Then we override the default values for the fields in the form.
         initial['category'] = Category.objects.order_by('pk').first() or Category.objects.create(name='Uncategorized')
         initial['author'] = self.request.user
@@ -28,5 +28,5 @@ class AddPostView(generic.CreateView):
 
 class UpdatePostView(generic.UpdateView):
     model = Post
+    form_class = UpdatePostForm # This line adds the form to the view.
     template_name = 'update_post.html'
-    fields = ['content']
