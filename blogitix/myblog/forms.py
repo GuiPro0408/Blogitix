dd099e2be80fabf27ebb5f3bc0c8ddbaf4b4722e
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-03-22 23:42:15 +0400

    styles of post update edited

--- blogitix/myblog/forms.py
+++ blogitix/myblog/forms.py
@@ -21,7 +21,7 @@ class UpdatePostForm(forms.ModelForm):
         model = Post # This line defines the model that will be used to create the form.
         fields = ('title', 'category', 'content') # This line defines the fields that will be displayed in the form.
         widgets = { # This line defines the type of widget to be used for each field.
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a title'}), # TextInput is a widget that allows the user to enter text.
-            'category': forms.Select(attrs={'class': 'form-control'}), # Select is a widget that allows the user to select a value from a list.
-            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter a text'})
+            'title': forms.TextInput(attrs={'class': 'form-control mt-2 text-secondary', 'placeholder': 'Enter a title'}), # TextInput is a widget that allows the user to enter text.
+            'category': forms.Select(attrs={'class': 'form-control mt-2 text-secondary'}), # Select is a widget that allows the user to select a value from a list.
+            'content': forms.Textarea(attrs={'class': 'form-control mt-2 text-secondary', 'placeholder': 'Enter a text'})
         }
